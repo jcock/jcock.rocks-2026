9900a60761112ffee247805cb46e97f031ef8b23
jcock <jason.cockerham@gmail.com>
2026-07-23 10:12:54 -0400

    Removed hash on work sample links... duh

--- components/modules/section/work.tsx
+++ components/modules/section/work.tsx
@@ -89,7 +89,7 @@ const SectionWork = ({
 							<motion.li key={sample.slug} variants={fadeUpItemVariants}>
 								<Card
 									as={Link}
-									href={`/work/${sample.slug}#work`}
+									href={`/work/${sample.slug}`}
 									className="relative pt-0"
 								>
 									<div className="aspect-4/3 overflow-hidden">
